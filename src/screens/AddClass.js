a03fe81795aaa2857ac6aa74118b377e1ea0ae83
freshy17 <freshy1388@gmail.com>
2026-02-21 22:56:04 +0700

    edit AddClass

--- src/screens/AddClass.js
+++ src/screens/AddClass.js
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer } from "react";
+import React, { useState, useContext } from "react";
 import {
   View,
   Text,
@@ -7,94 +7,171 @@ import {
   TouchableOpacity,
   Alert,
   Image,
+  Button,
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import TextFormInput from "../components/TextFormInput";
-import SelectFormInput from "../components/SelectFormInput";
+import { ClassContext } from "../context/ClassContext";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 
-const initialState = {
-  subject: '', 
-  code: '', 
-  room: '', 
-  date: '', 
-  starts: '', 
-  ends: '', 
-  error: ''
-}
+const AddClass = ({ navigation, route }) => {
+  const { dispatch } = useContext(ClassContext);
 
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case "UPDATE_FIELD":
-      return { ...state, [action.field]: action.value, error: "" };
-    case "RESET":
-      return initialState
+  const type = route?.params?.type ?? "class";
 
-    default:
-      return state;
+  const initialForm = {
+    subject: "",
+    code: "",
+    room: "",
+    date: null,
+    starts: null,
+    ends: null,
+    type,
   }
-};
 
-export default function AddClass({ navigation }) {
-
-  const [state, dispatch] = useReducer(formReducer, initialState)
+  const [form, setForm] = useState(initialForm);
 
   const handleSubmit = () => {
-     Alert.alert('Succeed', 'เพิ่มคลาสสำเร็จแล้ว', [
-      {
-        text: 'ตกลง',
-        onPress: () =>
-          navigation.navigate('DetailClass', {
-            classData: state
-          })
-      }
-  ])
-  dispatch({ type: 'RESET'})
-}
+    dispatch({ type: "ADD_CLASS", payload: { ...form, type: "exam" } });
+    navigation.goBack();
+  };
 
- const handleCancel = () => {
-  dispatch({ type: 'RESET'})
-}
+  const handleCancel = () => {
+    setForm(initialForm);
+  };
+
+  const showDatePicker = () => {
+    DateTimePickerAndroid.open({
+      value: form.date || new Date(),
+      onChange: (event, selectedDate) => {
+        if (selectedDate) {
+          setForm({ ...form, date: selectedDate });
+        }
+      },
+      mode: "date",
+      is24Hour: true,
+    });
+  };
+
+  const showStartTimePicker = () => {
+    DateTimePickerAndroid.open({
+      value: form.starts || new Date(),
+      onChange: (event, selectedTime) => {
+        if (selectedTime) {
+          setForm({ ...form, starts: selectedTime });
+        }
+      },
+      mode: "time",
+      is24Hour: true,
+    });
+  };
+
+  const showEndTimePicker = () => {
+    DateTimePickerAndroid.open({
+      value: form.starts || new Date(),
+      onChange: (event, selectedTime) => {
+        if (selectedTime) {
+          setForm({ ...form, ends: selectedTime });
+        }
+      },
+      mode: "time",
+      is24Hour: true,
+    });
+  };
+
+  const formatDate = (date) => {
+    if (!date) return "Select date";
+    return date.toLocaleDateString("th-TH");
+  };
+
+  const formatTime = (time) => {
+    if (!time) return "--:--";
+    return time.toLocaleTimeString("th-TH", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <Text style={styles.title}>Add Class</Text>
 
-        <Text style={styles.label}>Subject</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Subject"
-          value={state.subject}
-          onChangeText={(t) =>
-            dispatch({ type: "UPDATE_FIELD", field: "subject", value: t })
-          }
-        />
+        <View style={styles.form}>
+          <Text style={styles.label}>Subject</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Subject"
+            value={form.subject}
+            onChangeText={(t) => setForm({ ...form, subject: t })}
+          />
 
-        <Text style={styles.label}>Code</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Code"
-          value={state.code}
-          onChangeText={(t) =>
-            dispatch({ type: "UPDATE_FIELD", field: "code", value: t })
-          }
-        />
+          <Text style={styles.label}>Code</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Code"
+            value={form.code}
+            onChangeText={(t) => setForm({ ...form, code: t })}
+          />
 
-        <Text style={styles.label}>Room</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Room"
-          value={state.room}
-          onChangeText={(t) =>
-            dispatch({ type: "UPDATE_FIELD", field: "room", value: t })
-          }
-        />
+          <Text style={styles.label}>Room</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Room"
+            value={form.room}
+            onChangeText={(t) => setForm({ ...form, room: t })}
+          />
 
+          <Text style={styles.label}>Date</Text>
+          <TouchableOpacity 
+            style={[styles.input, styles.fakeDateInput]} 
+            onPress={showDatePicker}>
+            <Text style={[
+              styles.fakeInputText, 
+              form.date && styles.filledText
+              ]}>
+              {form.date ? formatDate(form.date) : "Select date"}
+            </Text>
+          </TouchableOpacity>
 
+          <Text style={styles.label}>Time</Text>
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <TouchableOpacity
+              style={[styles.input, styles.fakeTimeInput]}
+              onPress={showStartTimePicker}
+            >
+              <Text style={styles.fakeInputText}>
+                {form.starts ? formatTime(form.starts) : "Starts time"}
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[styles.input, styles.fakeTimeInput]}
+              onPress={showEndTimePicker}
+            >
+              <Text style={styles.fakeInputText}>
+                {form.ends ? formatTime(form.ends) : "Ends time"}
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <TouchableOpacity
+              style={styles.cancelButton}
+              onPress={handleCancel}
+            >
+              <Text style={styles.cancelButtonText}>Cancel</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.summitButton}
+              onPress={handleSubmit}
+            >
+              <Text style={styles.summitButtonText}>Summit</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -103,13 +180,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
   },
-  images: {
-    width: 80,
-    height: 80,
-    borderWidth: 2,
-    borderRadius: 75,
-    borderColor: "#ffffffff",
-    marginTop: 20,
+  input: {
+    backgroundColor: "#ffffff",
+    padding: 12,
+    borderRadius: 12,
+    marginBottom: 15,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
   },
   title: {
     fontSize: 20,
@@ -150,4 +229,23 @@ const styles = StyleSheet.create({
     color: "white",
     textAlign: "center",
   },
+  fakeDateInput: {
+    justifyContent: "center",
+    backgroundColor: "#fff", 
+    borderRadius: 12,
+  },
+  fakeTimeInput: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff", 
+    borderRadius: 12,
+  },
+  fakeInputText: {
+    color: "#817b7b", 
+  },
+  filledText: {
+    color: "#000000"
+  }
 });
+
+export default AddClass;
