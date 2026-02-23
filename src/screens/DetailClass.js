59d4b9061f0bd09b7508d042d7f7040bc1417c0c
taparis <sirapat.w@ku.th>
2026-02-24 01:49:34 +0700

    detailclass

--- src/screens/DetailClass.js
+++ src/screens/DetailClass.js
@@ -12,23 +12,94 @@ import {
 import { Ionicons } from "@expo/vector-icons";
 import { ClassContext } from "../context/ClassContext";
 
-const DetailClass = ({ navigation}) => {
+const formatDate = (date) => {
+  if (!date) return "-";
+  return new Date(date).toLocaleDateString("th-TH");
+};
+
+const getDayName = (date) => {
+  if (!date) return "";
+
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[new Date(date).getDay()];
+};
+
+const getDayColor = (date) => {
+  const day = new Date(date).getDay();
+
+  const colors = [
+    "#FFD6D6", // Sun
+    "#FFE66D", // Mon (เหลือง)
+    "#FFB3D1", // Tue (ชมพู)
+    "#B5EAD7", // Wed
+    "#C7CEEA", // Thu
+    "#FFDAC1", // Fri
+    "#E2F0CB", // Sat
+  ];
+
+  return colors[day];
+};
+
+const formatTime = (time) => {
+  if (!time) return "--:--";
+  return new Date(time).toLocaleTimeString("th-TH", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
+const DetailClass = ({ navigation }) => {
+
   const { classes } = useContext(ClassContext);
+
+  const sortedClasses = [...classes].sort((a, b) => {
+    if (!a.date) return 1;
+    if (!b.date) return -1;
+
+    const dayA = new Date(a.date).getDay();
+    const dayB = new Date(b.date).getDay();
+
+
+    if (dayA !== dayB) {
+      return dayA - dayB;
+    }
+
+
+    return new Date(a.starts) - new Date(b.starts);
+  });
+
 
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={classes}
+        data={sortedClasses}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
           <View style={styles.frame}>
+
+            <View
+              style={[
+                styles.dayBox,
+                { backgroundColor: getDayColor(item.date) }
+              ]}
+            >
+              <Text style={styles.date}>
+                {getDayName(item.date)}
+              </Text>
+              <Text style={styles.time}>
+                {formatTime(item.starts)} - {formatTime(item.ends)}
+              </Text>
+            </View>
+
+            {/* เพิ่มข้อมูลตารางเรียน */}
             <View style={styles.infoBox}>
               <Text style={styles.text}>{item.code}</Text>
               <Text style={styles.text}>{item.subject}</Text>
               <Text style={styles.text}>{item.room}</Text>
             </View>
 
+
+            {/* ปุ่มกดเพิ่มข้อมูลตารางเรียน   */}
             <TouchableOpacity
               style={styles.editButton}
               onPress={() =>
@@ -46,27 +117,49 @@ const DetailClass = ({ navigation}) => {
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 16,
+    fontSize: 18,
+    fontWeight: "bold",
     marginBottom: 5,
   },
   frame: {
     flexDirection: "row",
-    padding: 20,
-    borderWidth: 1,
-    borderRadius: 20,
-    borderColor: "pink",
     alignItems: "center",
-    backgroundColor: "pink",
-    marginTop: 20,
+    backgroundColor: "#EBD1D8",
+    marginHorizontal: 15,
+    marginTop: 15,
+    padding: 15,
+    borderRadius: 20,
+    position: "relative"
   },
   infoBox: {
     marginLeft: 15,
     flex: 1,
-    alignItems: "center",
+    justifyContent: "center",
+    //paddingRight: 35,
   },
   editButton: {
-    
+    position: "absolute",
+    top: 12,
+    right: 12,
   },
+  dayBox: {
+    width: 150,
+    height: 90,
+    borderRadius: 20,
+    padding: 10,
+    alignItems: "flex-start", 
+    justifyContent: "center", 
+  },
+  date: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  time: {
+    fontSize: 14,
+    marginTop: 5,
+    fontWeight: "500",
+  },
+
 });
 
 export default DetailClass;
