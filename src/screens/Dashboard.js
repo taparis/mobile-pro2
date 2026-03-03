fa1b51b606ea7108bb41e61a28212788bbfb482d
freshy17 <freshy1388@gmail.com>
2026-03-03 15:12:05 +0700

    delete 2 line

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -197,8 +197,6 @@ const Dashboard = () => {
             tasks.map((task) => (
               <View key={task.id} style={styles.taskbox}>
                 <Text style={styles.taskBoxlabel}>{task.desc}</Text>
-                <Text style={styles.taskBoxlabel}>{task.Date}</Text>
-                <Text style={styles.taskBoxlabel}>{task.Time}</Text>
               </View>
             ))
           )}
